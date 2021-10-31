dec5abaac1a1ae34f449de235ec7b504297a115d
Edo Wahdana <wahdanaedo@gmail.com>
2021-10-31 23:40:57 +0700

    Add tata cara pembayaran

--- application/views/main/Status.php
+++ application/views/main/Status.php
@@ -3,6 +3,23 @@
         redirect('c_home');
 ?>
 
+	<style>
+		ol.with-number {
+			margin: 0;
+			padding: 0;
+		}
+		
+		ol.with-number > li{
+			display: block;
+			margin-bottom: .5em;
+			margin-left: 2em;
+		}
+		
+		ol.with-number > li::before{
+			display: inline-block;
+		}
+	</style>
+
     <div class="row mt-4">
         <div class="col-sm-7 border-right">
             <h5 class="font-weight-bold text-center my-3" style="font-size: 20px;">Booking Status</h5>
@@ -96,6 +113,14 @@
         </div>
         <div class="col-sm-5">
             <h5 class="font-weight-bold text-center my-3" style="font-size: 20px;">Intruksi Pembayaran</h5>
+			<pre style="max-width: 500px; overflow-wrap: break-word; overflow: auto;">
+Ketentuan Pembayaran:
+<b>*</b>Harap Melakukan Pembayaran dengan Benar dan Teliti.
+<b>*</b>Pihak Pos Pendakian tidak bertanggung jawab atas kesalahan 
+ transfer atau kesalahan nominal transfer.
+<b>*</b>Setelah Melakukan Pembayaran, Uang Tidak dapat 
+ refund / dikembalikan.
+			</pre>
             <h4 style="font-size: 15px;">Pembayaran via Transfer</h4>
             <div class="row">
                 <div class="col-sm-3">
@@ -130,3 +155,47 @@
             </div>
         </div>
     </div>
+	
+	<hr class="container mt-4" style="border: 1px solid #c4c4c4;" />
+	
+	<div class="row my-4 px-4">
+		<div class="col-sm-4">
+			<h5 class="font-weight-bold text-center my-3" style="font-size: 15px;">Tata Cara pembayaran melalui Mesin ATM : </h5>
+			<ol class="list-group with-number">
+				<li class="list-group-item">Masukkan kartu ATM</li>
+				<li class="list-group-item">Pilih bahasa, sebaiknya pilih "Bahasa Indonesia" agar lebih mudah dipahami</li>
+				<li class="list-group-item">Masukkan nomor PIN ATM anda</li>
+				<li class="list-group-item">Pilih Transaksi lainnya, lalu pilih transfer</li>
+				<li  class="list-group-item">Pada pilihan bank tujuan transfer, pilih Bank sesuai tujuan</li>
+				<li class="list-group-item">Masukkan kode Bank dan nomor rekening tujuan</li>
+				<li class="list-group-item">Pastikan kembali kode Bank, nomor rekening dan nominal transfer benar</li>
+				<li class="list-group-item">Pilih benar dan konfirmasi kebenaran data a/n TNGC</li>
+				<li class="list-group-item">Tunggu hingga muncul keterangan transaksi anda berhasil</li>
+				<li class="list-group-item">Ambil kembali kartu ATM anda</li>
+			</ol>
+		</div>
+		<div class="col-sm-4">
+			<h5 class="font-weight-bold text-center my-3" style="font-size: 15px;">Tata Cara pembayaran melalui Mobile Banking : </h5>
+			<ol class="list-group with-number">
+				<li class="list-group-item">Login ke Mobile Banking dengan memasukkan User ID dan Password</li>
+				<li class="list-group-item">Pilih menu "Transfer" lalu pilih tujuan Bank</li>
+				<li class="list-group-item">Setelah itu lalu masukkan rekening tujuan dan email penerima jika ada</li>
+				<li class="list-group-item">Masukkan nominal yang ingin ditransfer, tambahkan keterangan transfer jika ada, lalu pilih "Lanjut"</li>
+				<li class="list-group-item">Setelah itu kamu akan diarahkan untuk memasukkan Password, jika sudah benar klik "Lanjut"</li>
+				<li class="list-group-item">Tunggu hingga transaksi berhasil</li>
+			</ol>
+		</div>
+		<div class="col-sm-4">
+			<h5 class="font-weight-bold text-center my-3" style="font-size: 15px;">Tata Cara pembayaran melalui Dompet Digital : </h5>
+			<ol class="list-group with-number">
+				<li class="list-group-item">Masuk ke aplikasi E-Wallet</li>
+				<li class="list-group-item">Masukkan PIN E-Wallet anda</li>
+				<li class="list-group-item">Pilih menu Transfer</li>
+				<li class="list-group-item">Pilih menu ke Rekening Bank</li>
+				<li class="list-group-item">Pilih Bank tujuan, nomor rekening dan keterangan transfer</li>
+				<li class="list-group-item">Pilih lanjutkan, dan konfirmasi kebenaran data a/n TNGC</li>
+				<li class="list-group-item">Pilih "Kirim"</li>
+				<li class="list-group-item">Tunggu hingga muncul pemberitahuan transfer berhasil</li>
+			</ol>
+		</div>
+	</div>
